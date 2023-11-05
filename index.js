35716d2783e47dcbb0e998c30321cab086a6137a
sittich0k <143976652+sittich0k@users.noreply.github.com>
2023-11-05 18:42:21 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+document.addEventListener("keydown", function (event) {
+  playSound(event.key);
+});
+
+// เล่นเสียงโดยตรงจากปุ่มคีย์บอร์ด
+function playSound(key) {
+  switch (key) {
+    case 'w':
+      var audio = new Audio('tom-1.mp3');
+      audio.play();
+      break;
+
+    case 'a':
+      var audio = new Audio('tom-2.mp3');
+      audio.play();
+      break;
+
+    case 's':
+      var audio = new Audio('tom-3.mp3');
+      audio.play();
+      break;
+
+    case 'd':
+      var audio = new Audio('tom-4.mp3');
+      audio.play();
+      break;
+
+    case 'j':
+      var audio = new Audio('crash.mp3');
+      audio.play();
+      break;
+
+    case 'k':
+      var audio = new Audio('kick-bass.mp3');
+      audio.play();
+      break;
+
+    case 'l':
+      var audio = new Audio('snare.mp3');
+      audio.play();
+      break;
+
+    default:
+      // หากคีย์ไม่ตรงกับรายการที่มีอยู่ ให้ไม่ทำอะไร
+      break;
+  }
+}
+
+  
+
